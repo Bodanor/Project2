7238d4e077b9ce119956b59ca7fb7621aeb54144
Liwinux <papadopoulos.chris@icloud.com>
2021-03-21 14:07:46 +0100

    Initial Commit

---
+++ lfsr/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lfsr
+//
+//  Created by Christos Papadopoulos on 21/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
